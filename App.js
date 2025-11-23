2b789e32d589a44352e6d813bbcb2abff1eecd85
Jeet Paresh Mehta <jpmehta@wisc.edu>
2025-11-23 02:33:36 -0600

    Work in progress on location services

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, View, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 
-export default function TabOneScreen() {
-  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+export default function App() {
+  const [location, setLocation] = useState(null);
   const [region, setRegion] = useState({
     latitude: 37.78825,
     longitude: -122.4324,
